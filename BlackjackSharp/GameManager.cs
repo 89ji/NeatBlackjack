47f485a1964c9bcd8428ee2eb66f31b357a34a0f
Joseph Bolduc <josephjittin@gmail.com>
2024-08-31 11:08:37 -0500

    did some work off camera (oops)

---
+++ BlackjackSharp/GameManager.cs
@@ -0,0 +1,86 @@
+﻿namespace BlackjackSharp;
+
+public enum GameResult
+{
+    Won,
+    Lost,
+    DoubleWon,
+    DoubleLost,
+    Surrendered,
+    Draw
+}
+public class GameManager
+{
+    readonly Hand dealerHand = new Hand();
+    readonly Hand playerHand = new Hand();
+    readonly IBlackjackPlayer dealer = new Dealer();
+    readonly IBlackjackPlayer player;
+
+    public GameManager(IBlackjackPlayer playerAgent)
+    {
+        player = playerAgent;
+    }
+
+    public GameResult PlayGame()
+    {
+        dealerHand.Reset();
+        playerHand.Reset();
+
+        dealerHand.AddCard(Card.GetRandomCard());
+        playerHand.AddCard(Card.GetRandomCard());
+        playerHand.AddCard(Card.GetRandomCard());
+        
+        bool surrenderable = true;
+        bool doubledDown = false;
+        bool surrendered = false;
+        while (true)
+        {
+            bool endGame = false;
+            switch (player.GetPlayerAction(playerHand, dealerHand, surrenderable))
+            {
+                case PlayerAction.Hit:
+                    playerHand.AddCard(Card.GetRandomCard());
+                    break;
+                case PlayerAction.Stand:
+                    endGame = true;
+                    break;
+                case PlayerAction.DoubleDown:
+                    playerHand.AddCard(Card.GetRandomCard());
+                    endGame = true;
+                    doubledDown = true;
+                    break;
+                case PlayerAction.Split:
+                    throw new NotImplementedException();
+                    break;
+                case PlayerAction.Surrender:
+                    endGame = true;
+                    surrendered = true;
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+            surrenderable = false;
+            if (endGame || playerHand.Evaluate() > 21) break;
+        }
+
+        while (dealer.GetPlayerAction(dealerHand, dealerHand) == PlayerAction.Hit) dealerHand.AddCard(Card.GetRandomCard());
+        
+        if(surrendered) return GameResult.Surrendered;
+        if (!doubledDown)
+        {
+            if (playerHand.Evaluate() > 21) return GameResult.Lost;
+            if (dealerHand.Evaluate() > 21) return GameResult.Won;
+            if (playerHand.Evaluate() > dealerHand.Evaluate()) return GameResult.Won;
+            if (dealerHand.Evaluate() == playerHand.Evaluate()) return GameResult.Draw;
+            return GameResult.Lost;
+        }
+        else
+        {
+            if (playerHand.Evaluate() > 21) return GameResult.DoubleLost;
+            if (dealerHand.Evaluate() > 21) return GameResult.DoubleWon;
+            if (playerHand.Evaluate() > dealerHand.Evaluate()) return GameResult.DoubleWon;
+            if (dealerHand.Evaluate() == playerHand.Evaluate()) return GameResult.Draw;
+            return GameResult.DoubleLost;
+        }
+    }
+}
---
+++ BlackjackSharpTester/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Diagnostics;
+using BlackjackSharp;
+
+namespace BlackjackSharpTester;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        /*IBlackjackPlayer me = new UIPlayer();
+        Hand testHand1 = new Hand();
+        Hand testHand2 = new Hand();
+        testHand1.AddCard(Card.GetRandomCard());
+        testHand1.AddCard(Card.GetRandomCard());
+        testHand2.AddCard(Card.GetRandomCard());
+        
+        me.GetPlayerAction(testHand1, testHand2);*/
+        
+        /*
+        GameManager man = new(new UIPlayer());
+        while(true) Console.WriteLine(man.PlayGame());
+        */
+
+        uint runIterations = 1000000;
+        Stopwatch watch = new();
+        watch.Restart();
+        
+        foreach(var userAgent in new IBlackjackPlayer[] {new RandomAction(), new StandOnly(), new Dealer()} )
+        {
+        Console.WriteLine($"\nDoing simulations for new thingy");
+        
+        GameManager man = new(userAgent);
+        
+        int[] results = new int[6]; 
+        for (int i = 0; i < runIterations; i++)
+        {
+            GameResult result = man.PlayGame();
+            results[(int)result]++;
+        }
+
+        watch.Stop();
+        Console.WriteLine($"Ran {runIterations} times in {watch.ElapsedMilliseconds} ms.");
+        
+        for (int i = 0; i < results.Length; i++)
+        {
+            Console.WriteLine($"{(GameResult)i}: \t{results[i]} \t {100 * (double)results[i]/(double)runIterations :F2}%");
+        }
+        }
+        
+        //Console.ReadLine();
+    }
+}
